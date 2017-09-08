24d674ff36aad239b33d55a3dcce5845a388eae9
Antonio Pio Maggio <antoniopio.maggio@live.it>
2017-09-08 18:11:57 +0200

    third commit

--- client.py
+++ client.py
@@ -1,37 +1,132 @@
 import socket
 import time
 import json
+import datetime
 
 import numpy as np
 import cv2
 import base64
+import pygame
 
-camera = cv2.VideoCapture(0)
-'''
-camera.set(3,320)
-camera.set(4,160)
-'''
-retval, image = camera.read()
+import csv, os
 
-# Initialize client socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server = 'localhost'
+server = '192.168.1.17'
 port = 5555
 
-# Connect to server
-s.connect((server, port))
+path = '/media/pi/VOLUME'
+file = 'sessionLog.csv'
+csvfile = None
 
-while True :
-    result =  s.recv(25).decode('utf-8')
-    data_loaded = json.loads(result)
+loop_0 = True
+run_0 = False
+run_1 = False
+isStop = True
 
-    retval, image = camera.read()   
+p_frame = False
 
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-    retval, buffer = cv2.imencode('.jpg', image, encode_param)
-    jpg_as_text = base64.b64encode(buffer)
+def main():
+    # Initialize procedure
+    cap, s, clock = init()
 
-    s.sendall(jpg_as_text)
+    # Connect to server
+    s.connect((server, port))
+
+    while loop_0:
+        # Recive joystick data
+        try:
+            j_data = get_jData()
+        except:
+            loop_0 = False
+
+        # Read image from camera
+        retval, image = camera.read()
+
+        # Send frame
+        try:
+            sendFrame(image)
+        except:
+            loop_0 = False
+
+        if p_frame and not j_data[3]:
+            run_1 = not run_1
+
+        if run_1:
+            path_ = init_record()
+            record(image, j_data, path_)
+        else:
+            stopRec()
+        
+        p_frame = j_data[3] 
+
+        print(data_loaded)
     
-    print(data_loaded)
+        clock.tick(15)
+
+    camera.release()
+
+def init(res = (403, 247)):
+    # Initialize pygame stuff
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    # Initialize opencv stuff
+    width, height = res
+    camera = cv2.VideoCapture(0)
+    camera.set(3,width)
+    camera.set(4,height)
+    camera.set(int(cv2.CAP_PROP_FPS), 10)
+
+    # Initialize client socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    return (camera, s, clock);
+
+def get_jData():
+    # Recieve data from server
+    result =  s.recv(25).decode('utf-8')
+
+    # Convert it into an object
+    joystick_data = json.loads(result)
+    return joystick_data;
+
+def sendFrame(image, quality = 60):
+    # Define encode param and encoding image into .jpg format
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 60]
+    retval, buffer = cv2.imencode('.jpg', image, encode_param)
+
+    #Send all data converted
+    s.sendall(buffer)
+    return;
+
+def init_record():
+    if run_0:
+        date_ = datetime.datetime.now() 
+        dir_ = path + 'dataLog:' + date[:-7].replace(' ', '_', 1)
+        os.makedirs(dir_)
+        os.makedirs(dir_ + 'IMGs')
+        csvfile = open(file, 'w', newline='')
+        run_0 = False
+        isStop = False
+        return 'G:\\' + dir_ + 'IMGs';
+    else:
+        return;
+    
+    
+def record(image, j_data, path_):
+    date = datetime.datetime.now()
+    file_name = path + '\\center_' + date.replace(' ', '_', 1) + '.jpg'
+    img = cv2.resize(image, (320, 196), interpolation = cv2.INTER_CUBIC)
+    cv2.imwrite(file_name, img)
+    csvwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow([file_name, j_data[0], j_data[1], j_data[2]])
+    return;
+
+def stopRec():
+    if not isStop:
+        isStop = True
+        csvfile.close()
+    return;
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -8,7 +8,6 @@ import numpy as np
 import cv2
 import base64
 
-
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -39,25 +38,36 @@ def threaded_client(conn):
         name = joystick.get_name()
         axes = joystick.get_numaxes() - 2
         
-        axes_val = [0]*axes
+        axes_val = [0]*(axes + 2)
 
         for i in range( axes ):
             axis = joystick.get_axis(i)
             axes_val[i] = round(axis*-1, 2)
+
+        if axes_val[1] < 0:
+            axes_val[1] = 0
+            axes_val[2] = 1
+        else:
+            axes_val[2] = 0
+
+        axes_val[3] = joystick.get_button(1)
         
         data_string = json.dumps(axes_val)
         conn.send(str.encode(data_string))
         
-        data = conn.recv(50240)
+        data = conn.recv(20000)
         # Convert back to binary
-        jpg_original = base64.b64decode(data)
+        
+        #jpg_original = base64.b64decode(data)
         # Write to a file to show conversion worked
-        np_array = np.fromstring(jpg_original, np.uint8)
+        np_array = np.fromstring(data, np.uint8)
         img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-
-        cv2.imshow("Cam", img)
-
-        clock.tick(20)
+        
+        height, width = img.shape[:2]
+        res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+        cv2.imshow("Cam", res)
+        
+        clock.tick(30)
         
     cv2.destroyAllWindows()
     conn.close()
@@ -66,7 +76,7 @@ def threaded_client(conn):
 while True:
 
     conn, addr = s.accept()
-    print('Connected to: '+addr[0]+':'+str(addr[1]))
+    print('Connected to: '+ addr[0] +':'+str(addr[1]))
 
     start_new_thread(threaded_client,(conn,))
 
